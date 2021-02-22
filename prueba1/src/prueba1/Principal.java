652a039a622a752cf3a7829f2e283463f609d754
Usu2 Name <usu2@example.com>
2021-02-22 16:17:48 +0100

    cambio

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,7 +3,7 @@ package prueba1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo ava-git");
 
 	}
 
